2de348909f1934bf6c18212d69694e16ecf48e8c
longhao1213 <1281578910@qq.com>
2020-04-27 17:15:28 +0800

    add

---
+++ src/main/java/com/lh/Application.java
@@ -0,0 +1,13 @@
+package com.lh;
+
+
+/**
+ * Copyright (C), 2006-2010, ChengDu ybya info. Co., Ltd.
+ * FileName: Application.java
+ *
+ * @author lh
+ * @version 1.0.0
+ * @Date 2020/04/27 17:14
+ */
+public class Application {
+}
