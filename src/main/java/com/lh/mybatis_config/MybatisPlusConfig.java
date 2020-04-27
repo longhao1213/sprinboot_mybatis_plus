39f4bb7286b593e51d2e1300e1cc6d7f1042b1df
longhao1213 <1281578910@qq.com>
2020-04-27 17:25:05 +0800

    add

---
+++ src/main/java/com/lh/mybatis_config/MybatisPlusConfig.java
@@ -0,0 +1,53 @@
+package com.lh.mybatis_config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import com.github.pagehelper.PageHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
+
+/**
+ * Copyright (C), 2006-2010, ChengDu ybya info. Co., Ltd.
+ * FileName: MybatisPlusConfig.java
+ *
+ * @author lh
+ * @version 1.0.0
+ * @Date 2020/04/24 15:43
+ */
+@Configuration
+@EnableTransactionManagement
+public class MybatisPlusConfig {
+    private final static Logger logger = LoggerFactory.getLogger(MybatisPlusConfig.class);
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+
+        return page;
+    }
+
+    @Bean
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor page = new PerformanceInterceptor();
+        page.setFormat(true);
+        return page;
+    }
+
+    //配置mybatis的分页插件pageHelper
+    @Bean
+    public PageHelper pageHelper(){
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum","true");
+        properties.setProperty("rowBoundsWithCount","true");
+        properties.setProperty("reasonable","true");
+        properties.setProperty("dialect","mysql");    //配置mysql数据库的方言
+        pageHelper.setProperties(properties);
+        return pageHelper;
+    }
+}
